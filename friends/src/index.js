dafb99521263ac4c7d90aa5c84a4e28d00a668c7
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-06-20 14:51:43 +0100

    setup root reducer

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+// import logger from 'redux-logger';
+import rootReducer from './reducers'
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ),
+)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
